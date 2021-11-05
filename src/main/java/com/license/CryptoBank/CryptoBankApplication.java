0a0286dcdf7ad7da56381c093c0285e2fd7a3ad4
Danyelss <mos.dani@yahoo.com>
2021-11-05 20:54:31 +0200

    Project added

---
+++ src/main/java/com/license/CryptoBank/CryptoBankApplication.java
@@ -0,0 +1,13 @@
+package com.license.CryptoBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/license/CryptoBank/CryptoBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.license.CryptoBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
