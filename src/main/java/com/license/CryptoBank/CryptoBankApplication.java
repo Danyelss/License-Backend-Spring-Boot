37e2c671daac6d50081a828eba165d69bd9f3644
Danyelss <mos.dani@yahoo.com>
2021-11-22 17:59:03 +0200

    Retring retring changes

--- src/main/java/com/license/CryptoBank/CryptoBankApplication.java
+++ src/main/java/com/license/CryptoBank/CryptoBankApplication.java
@@ -27,47 +27,7 @@ public class CryptoBankApplication {
         return new BCryptPasswordEncoder();
     }
 
-    /*@Bean
-    CommandLineRunner run(UserService userService, BalancesService balancesService) {
-        return args -> {
-            userService.saveRole(new Role(null, "ROLE_USER"));
-            userService.saveRole(new Role(null, "ROLE_MANAGER"));
-            userService.saveRole(new Role(null, "ROLE_ADMIN"));
-            userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
 
-            userService.saveUser(new User(null, "cosmin", "0000", "Pop", "Cosmin", "comsin@encode.com", "07232322", new ArrayList<>()));
-            userService.saveUser(new User(null, "beni", "0000", "Beniamin", "Cigher", "beni@encode.com", "07424222", new ArrayList<>()));
-            userService.saveUser(new User(null, "paula", "2002", "Paula", "Nelli", "pimonca@yahoo.com", "074342322", new ArrayList<>()));
-            userService.saveUser(new User(null, "andrei", "0000", "Bretea", "Andrei", "andrei@armata.com", "0725435432", new ArrayList<>()));
-
-            userService.addRoleToUser("cosmin", "ROLE_USER");
-            userService.addRoleToUser("beni", "ROLE_ADMIN");
-            userService.addRoleToUser("paula", "ROLE_USER");
-            userService.addRoleToUser("andrei", "ROLE_USER");
-
-            List<String> ETHTestLog = new ArrayList<>();
-            ETHTestLog.add("this");
-            ETHTestLog.add("is");
-            ETHTestLog.add("a");
-            ETHTestLog.add("test");
-            ETHTestLog.add("log");
-
-            List<String> FIATTestLog = new ArrayList<>();
-            FIATTestLog.add("FIAT");
-            FIATTestLog.add("this");
-            FIATTestLog.add("is");
-            FIATTestLog.add("a");
-            FIATTestLog.add("test");
-            FIATTestLog.add("log");
-            FIATTestLog.add("on");
-            FIATTestLog.add("FIAT");
-
-            balancesService.saveBalance(new Balances(null, "cosmin", 0, 0, ETHTestLog, FIATTestLog));
-            balancesService.saveBalance(new Balances(null, "beni", 1, 1, ETHTestLog, FIATTestLog));
-            balancesService.saveBalance(new Balances(null, "paula", 2.5, 2.3, ETHTestLog, FIATTestLog));
-
-        };
-    } */
 }
 
 
