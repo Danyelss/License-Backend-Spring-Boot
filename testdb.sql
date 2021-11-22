c970c7d0df78dc5115dc0545bad1cf8780c841cb
Danyelss <mos.dani@yahoo.com>
2021-11-22 12:08:53 +0200

    Unknown changes

---
+++ testdb.sql
@@ -0,0 +1,331 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: testdb
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `balances`
+--
+
+DROP TABLE IF EXISTS `balances`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `balances` (
+  `id` bigint NOT NULL,
+  `eth_bal` double NOT NULL,
+  `fiat_bal` double NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `balances`
+--
+
+LOCK TABLES `balances` WRITE;
+/*!40000 ALTER TABLE `balances` DISABLE KEYS */;
+INSERT INTO `balances` VALUES (9,0,0,'cosmin'),(10,1,1,'beni'),(11,2.5,2.3,'paula');
+/*!40000 ALTER TABLE `balances` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `balances_eth_transaction_logs`
+--
+
+DROP TABLE IF EXISTS `balances_eth_transaction_logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `balances_eth_transaction_logs` (
+  `balances_id` bigint NOT NULL,
+  `eth_transaction_logs` varchar(255) DEFAULT NULL,
+  KEY `FK9ls4xt7p4hxrcrd657nv7hn4o` (`balances_id`),
+  CONSTRAINT `FK9ls4xt7p4hxrcrd657nv7hn4o` FOREIGN KEY (`balances_id`) REFERENCES `balances` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `balances_eth_transaction_logs`
+--
+
+LOCK TABLES `balances_eth_transaction_logs` WRITE;
+/*!40000 ALTER TABLE `balances_eth_transaction_logs` DISABLE KEYS */;
+INSERT INTO `balances_eth_transaction_logs` VALUES (9,'this'),(9,'is'),(9,'a'),(9,'test'),(9,'log'),(10,'this'),(10,'is'),(10,'a'),(10,'test'),(10,'log'),(11,'this'),(11,'is'),(11,'a'),(11,'test'),(11,'log');
+/*!40000 ALTER TABLE `balances_eth_transaction_logs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `balances_fiat_transaction_logs`
+--
+
+DROP TABLE IF EXISTS `balances_fiat_transaction_logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `balances_fiat_transaction_logs` (
+  `balances_id` bigint NOT NULL,
+  `fiat_transaction_logs` varchar(255) DEFAULT NULL,
+  KEY `FKncmhwp4ql8dmao4gaa0g7ugkm` (`balances_id`),
+  CONSTRAINT `FKncmhwp4ql8dmao4gaa0g7ugkm` FOREIGN KEY (`balances_id`) REFERENCES `balances` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `balances_fiat_transaction_logs`
+--
+
+LOCK TABLES `balances_fiat_transaction_logs` WRITE;
+/*!40000 ALTER TABLE `balances_fiat_transaction_logs` DISABLE KEYS */;
+INSERT INTO `balances_fiat_transaction_logs` VALUES (9,'FIAT'),(9,'this'),(9,'is'),(9,'a'),(9,'test'),(9,'log'),(9,'on'),(9,'FIAT'),(10,'FIAT'),(10,'this'),(10,'is'),(10,'a'),(10,'test'),(10,'log'),(10,'on'),(10,'FIAT'),(11,'FIAT'),(11,'this'),(11,'is'),(11,'a'),(11,'test'),(11,'log'),(11,'on'),(11,'FIAT');
+/*!40000 ALTER TABLE `balances_fiat_transaction_logs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ethaddress`
+--
+
+DROP TABLE IF EXISTS `ethaddress`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ethaddress` (
+  `id` bigint NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `balance` double NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ethaddress`
+--
+
+LOCK TABLES `ethaddress` WRITE;
+/*!40000 ALTER TABLE `ethaddress` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ethaddress` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hibernate_sequence`
+--
+
+DROP TABLE IF EXISTS `hibernate_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+LOCK TABLES `hibernate_sequence` WRITE;
+/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
+INSERT INTO `hibernate_sequence` VALUES (13);
+/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `refreshtoken`
+--
+
+DROP TABLE IF EXISTS `refreshtoken`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `refreshtoken` (
+  `id` bigint NOT NULL,
+  `expiry_date` datetime NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `user_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_or156wbneyk8noo4jstv55ii3` (`token`),
+  KEY `FKa652xrdji49m4isx38pp4p80p` (`user_id`),
+  CONSTRAINT `FKa652xrdji49m4isx38pp4p80p` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `refreshtoken`
+--
+
+LOCK TABLES `refreshtoken` WRITE;
+/*!40000 ALTER TABLE `refreshtoken` DISABLE KEYS */;
+/*!40000 ALTER TABLE `refreshtoken` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role` (
+  `id` bigint NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'ROLE_USER'),(2,'ROLE_MANAGER'),(3,'ROLE_ADMIN'),(4,'ROLE_SUPER_ADMIN');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaction_logs`
+--
+
+DROP TABLE IF EXISTS `transaction_logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transaction_logs` (
+  `id` bigint NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaction_logs`
+--
+
+LOCK TABLES `transaction_logs` WRITE;
+/*!40000 ALTER TABLE `transaction_logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transaction_logs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` bigint NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (5,'comsin@encode.com','Pop','Cosmin','$2a$10$KisNZQRnnWQ/Z4MVH6hc0eS0/EilMiXGMKgjLWfG2T186jgHOhyDi','07232322','cosmin'),(6,'beni@encode.com','Beniamin','Cigher','$2a$10$eWNBsBDqw0DAV8bUcKSPtezcuXsqDhPWd6F5RFKOk9ty9pnOrTyEy','07424222','beni'),(7,'pimonca@yahoo.com','Paula','Nelli','$2a$10$2uqDdDwslOhWG9Fyz.jyR.i4RdbT7muwW5eaYZFGcvnoyHFikgCE2','074342322','paula'),(8,'andrei@armata.com','Bretea','Andrei','$2a$10$EEtENk4cvS1/bCbRPhs5t.icMC5.3q59lV77brRxVjgowte9UIdsC','0725435432','andrei'),(12,'comsin@encode.com','Psdadsaop','Codsadsadadsasmin','$2a$10$8gS9gqjZrp/RApyHgT15heWgfjUBWadI7Ke9ZttrN3F2AVrMxH6sm','07232322','cosdsadasdamadsain');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_roles`
+--
+
+DROP TABLE IF EXISTS `user_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_roles` (
+  `user_id` bigint NOT NULL,
+  `roles_id` bigint NOT NULL,
+  KEY `FKj9553ass9uctjrmh0gkqsmv0d` (`roles_id`),
+  KEY `FK55itppkw3i07do3h7qoclqd4k` (`user_id`),
+  CONSTRAINT `FK55itppkw3i07do3h7qoclqd4k` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FKj9553ass9uctjrmh0gkqsmv0d` FOREIGN KEY (`roles_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+LOCK TABLES `user_roles` WRITE;
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+INSERT INTO `user_roles` VALUES (5,1),(6,3),(7,1),(8,1),(12,1);
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(120) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UKr43af9ap4edm43mmtq01oddj6` (`username`),
+  UNIQUE KEY `UK6dotkott2kjsp8vw4d0m25fb7` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-22 11:42:36
