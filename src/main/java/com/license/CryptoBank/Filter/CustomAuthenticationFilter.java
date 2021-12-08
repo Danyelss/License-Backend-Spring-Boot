5c38603662246c61098c22af7dc34f13b1cdb204
Danyelss <mos.dani@yahoo.com>
2021-12-08 21:29:35 +0200

    server error

--- src/main/java/com/license/CryptoBank/Filter/CustomAuthenticationFilter.java
+++ src/main/java/com/license/CryptoBank/Filter/CustomAuthenticationFilter.java
@@ -21,7 +21,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Slf4j
